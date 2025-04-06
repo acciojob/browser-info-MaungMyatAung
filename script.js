f477e79a00e6c44ed3c01d48f921cf20bdca0bbb
MaungMyatAung <85542468+MaungMyatAung@users.noreply.github.com>
2025-04-06 13:43:45 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your JS code here. If required.
+// Get references to browser details
+    const browserName = navigator.appName;
+    const browserVersion = navigator.appVersion;
+
+    // Construct message
+    const message = "You are using " + browserName + " version " + browserVersion;
+
+    // Display message in the div
+    document.getElementById("browser-info").textContent = message;
